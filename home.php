2cb42c54b46aa1d1777526d58f48f78ff1b965c3
Josuna10 <whwhtres03@gmail.com>
2024-09-18 18:34:15 -0500

    first commit

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php
+    require_once('./includes/header.php');
+?>
+    <main>
+        <h2 class="white">Bienvenidos a la pagina</h2>
+        <hr>
+        <section class="white">
+            <p>Estás logeado con éxito</p>
+            <p>Puedes ver la informacio aqui</p>
+        </section>
+    </main>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<?php
+    ob_start();
+    session_start();
+
+    if(!isset($_SESSION['valid'])){
+        header('Location: index.php');
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./ressources/css/styles.css"/>
+    <title>Login con PHP y Sessions</title>
+</head>
+<body>
+    <header>
+        <div id="menu-user">
+            <p>Hola <?php echo $_SESSION['email'];?> !</p>
+            <a href="logout.php" id="logout-link">Cerrar sesion</a>
+        </div>
+    </header>
